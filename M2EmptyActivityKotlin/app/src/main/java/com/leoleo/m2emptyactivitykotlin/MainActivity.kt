fa4bd51cd5be4344442b6e7562a3251ec2de1cc4
LeoAndo <leo.777.info@gmail.com>
2023-02-13 13:51:02 +0900

    update M2EmptyActivityKotlin

---
+++ M2EmptyActivityKotlin/app/src/main/java/com/leoleo/m2emptyactivitykotlin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.leoleo.m2emptyactivitykotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
